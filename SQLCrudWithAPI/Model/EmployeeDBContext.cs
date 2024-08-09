c789416444f731092bdc900eae2de4acd97f40c2
Brijendra Sharma <brijendra.sharma@nagarro.com>
2024-08-09 19:09:19 +0530

    Added Entity Class and Entity Context in Model, Removed boilerplate controller and model

---
+++ SQLCrudWithAPI/Model/EmployeeDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SQLCrudWithAPI.Model
+{
+    public class EmployeeDBContext:DbContext
+    {
+        public EmployeeDBContext(DbContextOptions<EmployeeDBContext> opt):base(opt)
+        {
+            
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
